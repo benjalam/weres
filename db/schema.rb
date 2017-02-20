61612f5dcfc4c7e9f7522548716385dd12fed539
Benjamin Lambrou <lambroubenjamin@gmail.com>
2017-02-20 15:25:19 +0100

    creation of job_offers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220142210) do
+ActiveRecord::Schema.define(version: 20170220142324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20170220142210) do
     t.string   "website"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "job_offers", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_job_offers_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,5 +53,6 @@ ActiveRecord::Schema.define(version: 20170220142210) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "job_offers", "users"
   add_foreign_key "users", "companies"
 end

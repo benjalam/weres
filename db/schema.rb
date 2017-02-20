5957c327a275cf4717a99476c70a6077132f2802
Benjamin Lambrou <lambroubenjamin@gmail.com>
2017-02-20 15:26:00 +0100

    creation of candidates

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220142324) do
+ActiveRecord::Schema.define(version: 20170220142535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "candidates", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.integer  "job_offer_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["job_offer_id"], name: "index_candidates_on_job_offer_id", using: :btree
+    t.index ["user_id"], name: "index_candidates_on_user_id", using: :btree
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -53,6 +63,8 @@ ActiveRecord::Schema.define(version: 20170220142324) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "candidates", "job_offers"
+  add_foreign_key "candidates", "users"
   add_foreign_key "job_offers", "users"
   add_foreign_key "users", "companies"
 end

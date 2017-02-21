c51929eb1229593a196a1ea7100d0d0d1e0b9679
Leslie Maarek <maarek.leslie@gmail.com>
2017-02-21 15:36:49 +0100

    change models again + seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   belongs_to :company
-  has_many :job_offers
+  has_many :job_offers, dependent: :nullify
+  has_many :candidates, dependent: :nullify
 end

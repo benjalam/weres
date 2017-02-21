e7d9649627ede46354835a5e64f40bf57e36fb73
Federico Volonteri <federico.volonteri@gmail.com>
2017-02-21 12:46:57 +0100

    Contacted controller + candidate controller

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,20 +1,17 @@
-class MatchesController < ApplicationController
-  def index
-
-
+class ContactedCandidatesController < ApplicationController
+  def new
+    @contacted_candidate = ContactedCandidate.new
   end
 
-  def show
-    @match = Match.find(params[:id])
+  def create
+    @contacted_candidate = ContactedCandidate.new
+    @candidate = Candidate.find(params[:id])
+    @contacted_candidate.candidate = @candidate
     @job_offer = JobOffer.find(params[:job_offer_id])
-    @candidate = Candidate.find(@match.candidate_id)
-  end
-
-  def edit
-
-  end
-
-  def update
-
+    @contacted_candidate.job_offer = @job_offer
+    @company = Company.find(params[:company_id])
+    @contacted_candidate.company = @company
+    @contacted_candidate.save
+    redirect_to company_job_offer_candidates_path(@company, @job_offer)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :matches, only: [:new, :create]
   resources :companies, only: [:show, :edit, :update] do
     resources :job_offers do
       resources :candidates, only: [:show, :index]

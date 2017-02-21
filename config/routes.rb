1944cc044ba59f3737828967ebddb72b594c1b97
Federico Volonteri <federico.volonteri@gmail.com>
2017-02-21 11:34:51 +0100

    Added match model + migration + association

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :candidates, only: [:show]
   resources :companies, only: [:show, :edit, :update] do
-    resources :candidates, only: [:show, :index]
+    resources :matches, only: [:show, :index, :edit, :update]
   end
 end

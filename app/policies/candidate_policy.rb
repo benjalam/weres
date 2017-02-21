7394e273c7b2ae080f002a1910dd7c523b9dd634
Leslie Maarek <maarek.leslie@gmail.com>
2017-02-21 16:49:23 +0100

    views de candidate

--- app/policies/candidate_policy.rb
+++ app/policies/candidate_policy.rb
@@ -1,7 +1,15 @@
 class CandidatePolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope
+      scope.all
     end
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
   end
 end

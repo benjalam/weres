a5d1f1a17b6679c8457ab7ac8669f279958ebb8a
Federico Volonteri <federico.volonteri@gmail.com>
2017-02-24 13:46:59 +0100

    admin user custom

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -14,6 +14,16 @@ ActiveAdmin.register User do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+   form do |f|
+    inputs do
+        f.input :first_name
+        f.input :last_name
+        f.input :company, as: :select, collection: Company.all.map {|c| "#{c.name}" }
+        f.input :email
+        f.input :password
 
+    end
+    actions
+  end
 
 end

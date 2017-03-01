413e391858504da9eb883bfd2eb3ac84a9e99e1b
Julie Ehrmann <julie.ehrmann1@gmail.com>
2017-03-01 17:19:27 +0100

    Corrected the tab bug

--- app/assets/javascripts/fav.js
+++ app/assets/javascripts/fav.js
@@ -1,5 +1,5 @@
- $(document).ready(function() {
-   $(".favourite, .favourited").click(function(){
-     $(this).toggleClass("favourited")
-   });
-  });
+ // $(document).ready(function() {
+ //   $(".favourite, .favourited").click(function(){
+ //     $(this).toggleClass("favourited")
+ //   });
+ //  });

96377402ee5ecbc5c5b6b37ee2d84161895fda93
Benjamin Lambrou <lambroubenjamin@gmail.com>
2017-04-10 10:31:04 +0200

    change DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307174147) do
+ActiveRecord::Schema.define(version: 20170406144128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20170307174147) do
     t.datetime "updated_at",   null: false
     t.string   "position"
     t.string   "email"
+    t.string   "category"
     t.index ["job_offer_id"], name: "index_candidates_on_job_offer_id", using: :btree
     t.index ["user_id"], name: "index_candidates_on_user_id", using: :btree
   end
